2752f577f553663e351958d575b3c4e72b2ad60d
thomasHM21 <thomas.forsberg@hm.com>
2021-02-12 09:52:46 +0100

    Added Player class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,7 +23,23 @@ public class Main {
     }
     private static void startGame() throws IOException, InterruptedException {
         Terminal terminal = createTerminal();
-        drawRoadside(terminal, 15);
+        int roadSideWidth = 20;
+        drawRoadside(terminal, roadSideWidth);
+        Player player = new Player(40,24,'X');
+        //Add enemies
+        drawCars(terminal,player);
+
+        do {
+            KeyStroke keyStroke = getUserKeyStroke(terminal);
+
+            movePlayer(player, keyStroke,roadSideWidth);
+
+            //moveMonsters(player, monsters);
+
+            drawCars(terminal, player);
+
+        } while (true);
+
     }
 
     private static Terminal createTerminal() throws IOException {
@@ -29,6 +47,41 @@ public class Main {
         Terminal terminal = terminalFactory.createTerminal();
         terminal.setCursorVisible(false);
         return terminal;
+    }
+
+    private static void movePlayer(Player player, KeyStroke keyStroke, int roadSideWidth) {
+        switch (keyStroke.getKeyType()) {
+            case ArrowLeft:
+                if (player.getX() > roadSideWidth){
+                    player.moveLeft();
+                }
+                break;
+            case ArrowRight:
+                if (player.getX() < (79-roadSideWidth)){
+                    player.moveRight();
+                }
+                break;
+        }
+    }
+
+    private static KeyStroke getUserKeyStroke(Terminal terminal) throws InterruptedException, IOException {
+        KeyStroke keyStroke;
+        do {
+            Thread.sleep(5);
+            keyStroke = terminal.pollInput();
+        } while (keyStroke == null);
+        return keyStroke;
+    }
+
+    private static void drawCars(Terminal terminal, Player player) throws IOException {
+        terminal.setCursorPosition(player.getPreviousX(), player.getPreviousY());
+        terminal.putCharacter(' ');
+
+        terminal.setForegroundColor(TextColor.ANSI.RED);
+        terminal.setCursorPosition(player.getX(), player.getY());
+        terminal.putCharacter(player.getSymbol());
+
+        terminal.flush();
     }
 
     private static void drawRoadside(Terminal terminal, int width) throws IOException {
---
+++ src/main/java/Player.java
@@ -0,0 +1,67 @@
+public class Player {
+    private int x;
+    private int y;
+    private char symbol;
+    private int previousX;
+    private int previousY;
+
+    public Player(int x, int y, char symbol) {
+        this.x = x;
+        this.y = y;
+        this.symbol = symbol;
+        this.previousX = x;
+        this.previousY = y;
+    }
+
+    public void moveLeft(){
+        previousX = x;
+        previousY = y;
+        x -= 1;
+    }
+
+    public void moveRight(){
+        previousX = x;
+        previousY = y;
+        x += 1;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getPreviousX() {
+        return previousX;
+    }
+
+    public void setPreviousX(int previousX) {
+        this.previousX = previousX;
+    }
+
+    public int getPreviousY() {
+        return previousY;
+    }
+
+    public void setPreviousY(int previousY) {
+        this.previousY = previousY;
+    }
+}
